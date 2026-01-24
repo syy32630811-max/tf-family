d13ecb32f3241e4bc09450e323f539838558f3f2
shiyingying <syy32630811@gmail.com>
2026-01-24 21:17:13 +0800

    feat: 丰富邮件内容

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@
  * @FilePath: /crystal_recognition_server/src/config/index.ts
  */
 export const EMAIL_CONFIG = {
-  host: 'imap.163.com', // 邮箱服务器地址
+  host: 'smtp.163.com', // 邮箱服务器地址
   port: 465, // 服务器端口 默认 465
   secure: true, // true for 465, false for other ports
   auth: {
--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -20,7 +20,7 @@ export class TimingTaskService {
      * 1950 时代少年团-棉花娃娃动物系列套装 时代少年团:贺峻霖款
      * 1944 时代少年团-棉花娃娃动物系列套装 时代少年团:马嘉祺款
      */
-    const notNeedSkuIds = [2751, 2758, 2757, 2756, 2755, 2754, 2753, 2752, 1733, 1732, 342, 340, 337]
+    const notNeedSkuIds = [2751, 2758, 2757, 2756, 2755, 2754, 2753, 2752, 1733, 1732, 342, 340, 337, 2815, 2816, 2817, 2818, 2819, 2820, 2821, 2822, 2823, 2824, 2825, 2826, 2827, 2828, 2829, 2830]
     const token = await this.authService.getToken();
     // 获取商品列表
     const goodsList = await this.goodService.getGoodsList(token);
@@ -28,6 +28,8 @@ export class TimingTaskService {
       return
     }
     let total = 0;
+    const orderGoodsArr = []
+    const handleGoodsArr = []
     for (const goods of goodsList) {
       // 获取商品详情
       const detail = await this.goodService.getGoodsDetail(goods.spuCode, token);
@@ -35,7 +37,7 @@ export class TimingTaskService {
       // 循环商品选项，先从HJL开始购买
       for (let i = length - 1; i >= 0; i--) {
         const item = detail.goodsSkuList[i]
-        // 删除库存为0的商品
+        // 过滤库存为0的商品
         if (item.stock === 0) {
           continue
         }
@@ -44,7 +46,13 @@ export class TimingTaskService {
           continue
         }
         // 校验商品是否支持购买，余量不足或者超过上限
-        const isVerify = await this.goodService.verification(item.id, token);
+        const verifyRst = await this.goodService.verification(item.id, token);
+        const isVerify = verifyRst[0]
+        if (length === 1) {
+          handleGoodsArr.push(`• ${item.skuName}, ${isVerify.message}`)
+        } else {
+          handleGoodsArr.push(`• ${detail.name}-${item.skuName}, ${isVerify.message}`)
+        }
         function isNullOrEmpty(str: string | null | undefined): boolean {
           return str === null || str === undefined || str === '';
         }
@@ -55,13 +63,20 @@ export class TimingTaskService {
             sellPrice: item.sellPrice,
           }, token)
           total++
+          if (length === 1) {
+            orderGoodsArr.push(`• ${item.skuName}`)
+          } else {
+            orderGoodsArr.push(`• ${detail.name}-${item.skuName}`)
+          }
         }
       }
     }
+    const orderGoodString = orderGoodsArr.join('\n')
+    const handleGoodString = handleGoodsArr.join('\n')
     await this.emailService.sendEmail({
       email: 'syy_3263@163.com', // 收件人
       subject: `TF 定时任务`,
-      text: `${new Date()} 定时任务执行完成, 购买了 ${total} 件商品`,
+      text: `${new Date()} 定时任务执行完成, 购买了 ${total} 件商品，详情如下：\n ${orderGoodString}\n 校验商品详情：\n ${handleGoodString}`,
     });
   }
 }
